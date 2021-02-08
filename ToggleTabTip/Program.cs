80278b1ab87e9fc9b726d1e22f1e37e9495726c5
Félix An <cutekids100@gmail.com>
2021-02-08 09:58:58 -0500

    Initial commit

---
+++ ToggleTabTip/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uiHostNoLaunch = new UIHostNoLaunch();
+            var tipInvocation = (ITipInvocation)uiHostNoLaunch;
+            tipInvocation.Toggle(GetDesktopWindow());
+            Marshal.ReleaseComObject(uiHostNoLaunch);
+        }
+
+        [ComImport, Guid("4ce576fa-83dc-4F88-951c-9d0782b4e376")]
+        class UIHostNoLaunch
+        {
+        }
+
+        [ComImport, Guid("37c994e7-432b-4834-a2f7-dce1f13b834b")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        interface ITipInvocation
+        {
+            void Toggle(IntPtr hwnd);
+        }
+
+        [DllImport("user32.dll", SetLastError = false)]
+        static extern IntPtr GetDesktopWindow();
+    }
+}
